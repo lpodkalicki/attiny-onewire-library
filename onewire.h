4bc2640f7c6b6a10c048a66a7cd384698777e9ad
Łukasz Podkalicki <lpodkalicki@gmail.com>
2018-08-11 20:55:10 +0200

    Refactor.

--- onewire.h
+++ onewire.h
@@ -13,11 +13,11 @@
 
 #include <stdint.h>
 
-#define	ONEWIRE_SEARCH_ROM	0xF0
-#define	ONEWIRE_READ_ROM	0x33
-#define	ONEWIRE_MATCH_ROM	0x55
-#define	ONEWIRE_SKIP_ROM	0xCC
-#define	ONEWIRE_ALARM_SEARCH	0xEC
+#define ONEWIRE_SEARCH_ROM	0xF0
+#define ONEWIRE_READ_ROM	0x33
+#define ONEWIRE_MATCH_ROM	0x55
+#define ONEWIRE_SKIP_ROM	0xCC
+#define ONEWIRE_ALARM_SEARCH	0xEC
 
 void onewire_init(uint8_t pin);
 uint8_t onewire_reset(void);
